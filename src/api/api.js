a4c5aa9e2653571833633c8521d6b523867dc238
Ahmet Hamdi CECEN <ahcecen@gmail.com>
2026-07-17 11:55:12 +0300

    baglantı kuruldu

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 // src/api/api.js
 import axios from "axios";
 
-const API_URL = "http://localhost:5141/api";
+const API_URL = process.env.REACT_APP_API_URL;
 const api = axios.create({
   baseURL: API_URL,
   headers: {
